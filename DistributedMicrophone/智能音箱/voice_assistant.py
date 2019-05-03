2931b02c8c413828b06398106f4adb4041b22148
zhujisheng <zhujisheng@gmail.com>
2019-05-03 23:08:06 +0800

    Update voice_assistant.py

--- DistributedMicrophone/智能音箱/voice_assistant.py
+++ DistributedMicrophone/智能音箱/voice_assistant.py
@@ -1,23 +1,30 @@
 #!/usr/bin/env python3
 
 import speech_recognition as sr
-from tcp_mic import TcpAudio
 from ha_cli import ha_cli
 from ais_cli import tuling123
 import logging
 import threading
-
-
 logging.basicConfig(format='[%(levelname)s] %(asctime)s %(message)s', level=logging.INFO)
 
-REMOTE_MIC_CONFIG = [('192.168.3.17',3344, "light.miclight")]
-LOCAL_MIC_CONFIG = True
+def RemoteMicCB(stage=1):
+    if stage==0:
+        ha.call_service('light', 'turn_off', data={"entity_id": "light.miclight"})
+    elif stage==1:
+        ha.call_service('light', 'turn_on', data={"entity_id": "light.miclight", "effect":"Strobe"})
+    elif stage==2:
+        ha.call_service('light', 'turn_on', data={"entity_id": "light.miclight", "brightness": 255, "effect":"None"})
 
-TULING_USER_ID = '407535'
-TULING_API_KEY = '0bee51f9bca64295a5c7f34b2eeb81ed'
+REMOTE_MIC_CONFIG = [('192.168.3.17',3344, RemoteMicCB)]
+LOCAL_MIC_CONFIG = True
 
 HA_URL = 'http://localhost:8123'
 HA_TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1NTA1ODY4MDIsImlzcyI6IjRlODRjNjJiYTI3NzQxNzA5ZjgyYWMzYWYzMjgzNzJmIiwiZXhwIjoxODY1OTQ2ODAyfQ.YK5X47ucA5Ug2NlRNa0VE7Z6bZruwthIdPzFAVT7eYA'
+TTS_SERVICE_NAME = 'baidu_say'
+MEDIA_PLAYER_NAME = 'all'
+
+TULING_USER_ID = '407535'
+TULING_API_KEY = '0bee51f9bca64295a5c7f34b2eeb81ed'
 
 SNOWBOY_LOCATION = '/home/pi/voice_assistant/sb/'
 SNOWBOY_MODELS = ['/home/pi/voice_assistant/sb/models/snowboy.umdl']
@@ -26,9 +33,6 @@ import sys
 sys.path.append(SNOWBOY_LOCATION)
 import snowboydecoder
 sys.path.pop()
-
-TTS_SERVICE_NAME = 'baidu_say'
-MEDIA_PLAYER_NAME = 'all'
 
 def LocalMicProcess():
     with sr.Microphone(sample_rate=16000) as source:
@@ -66,34 +70,20 @@ def LocalMicProcess():
                 logging.error("(LocalMicrophone)与HomeAssistant通讯失败：{0}".format(e))
                 continue
 
-
-def RemoteMicHotWordCB():
-    logging.info("(RemoteMicrophone)被唤醒...")
-    ha.call_service('light', 'turn_on', data={"entity_id": "light.miclight",
-                                              "effect":"Strobe"}
-                    )
-def RemoteMicProcess(MicIP, MicPort, MicLightID):
+def RemoteMicProcess(MicIP, MicPort, StageCallback):
     with TcpAudio(MicIP, MicPort) as source:
         logging.info("({0})加载远程麦克风...".format(MicIP))
         while True:
             try:
-                ha.call_service('light',
-                                'turn_off',
-                                data={"entity_id": MicLightID}
-                                )
+                StageCallback(0)
                 logging.info("({0})开始监听……".format(MicIP))
                 audio = r.listen(source,
                                  phrase_time_limit=6,
                                  snowboy_configuration=SNOWBOY_CONFIG,
-                                 hot_word_callback=RemoteMicHotWordCB,
+                                 hot_word_callback=StageCallback,
                                  min_time_after_hot_word=3
                                 )
-                ha.call_service('light',
-                                'turn_on',
-                                data={"entity_id": MicLightID,
-                                      "brightness": 255,
-                                      "effect":"None"}
-                                )
+                StageCallback(2)
                 logging.info("({0})开始识别……".format(MicIP))
 
                 recognized_text = r.recognize_google_cn(audio, language='zh-CN')
@@ -120,9 +110,11 @@ r = sr.Recognizer()
 ha = ha_cli(token=HA_TOKEN, base_url=HA_URL)
 tuling = tuling123(user_id=TULING_USER_ID, api_key=TULING_API_KEY)
 
-for config in REMOTE_MIC_CONFIG:
-    handler = threading.Thread(target=RemoteMicProcess, args=config)
-    handler.start()
+if REMOTE_MIC_CONFIG:
+    from tcp_mic import TcpAudio
+    for config in REMOTE_MIC_CONFIG:
+        handler = threading.Thread(target=RemoteMicProcess, args=config)
+        handler.start()
 
 if LOCAL_MIC_CONFIG:
     LocalMicProcess()
